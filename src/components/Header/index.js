ab11c6b9e2e1bd24cf0c536b97d29ca1011b61fd
raullude <rluquede@gmail.com>
2022-05-06 16:21:54 +0200

    primer commit y creación del header

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { CloseButton, Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Link } from "wouter";
+import "./index.css"
+
+export default function Header(){
+    return(
+        <Navbar bg="dark" expand="lg" fixed="top" sticky="top" className="navBar" >
+            <Container fluid>
+                <Navbar.Brand href="#">
+                    <img src="" alt="" fluid/>
+                </Navbar.Brand>
+                <Navbar.Toggle className="boton" aria-controls={`offcanvasNavbar-expand-lg`}/>
+                <Navbar.Offcanvas id={`offcanvasNavbar-expand-lg`} aria-labelledby={`offcanvasNavbarLabel-expand-lg`} placement="end" className="canvas">
+                    <Offcanvas.Header closeButton closeVariant="white">
+                        <Offcanvas.Title id={`offcanvasNavbar-expand-lg`}>
+                            Usuario
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3 enlaces">
+                        <Nav.Link to="/" className="enlace">Mis entradas</Nav.Link>
+                        <Nav.Link to="/" className="enlace">Cerrar Sesion</Nav.Link>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
+    );
+}
